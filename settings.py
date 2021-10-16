4e510b86ff7bae3e05841eb0ee8621c2b4e71f2d
Your Name <you@example.com>
2021-10-16 20:44:31 +0800

    Update setting.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XXX   ',
+' XX P                       ',
+' XXXX                   XXX ',
+' XXXX                       ',
+' XX    X  XXXXXX  XX  XXXX  ',
+'       X  XXXXXX  XX  XXXX  ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screen_width = 1550
+screen_height = 800
